a3d1627dccca842be7984543688b6c13adbba871
grzegorzwawrzen <grzegorz.wawrzen@protonmail.com>
2025-05-24 13:34:26 +0200

    chore: initialize project

---
+++ vocabulary-command-service/src/main/java/com/learnwords/vocabularycommandservice/VocabularyCommandServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learnwords.vocabularycommandservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabularyCommandServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VocabularyCommandServiceApplication.class, args);
+    }
+
+}
