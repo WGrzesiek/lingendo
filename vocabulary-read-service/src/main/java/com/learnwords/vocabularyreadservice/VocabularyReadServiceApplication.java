1e2a34d0bb423974ba61b6e536c05e718b0f23ac
grzegorzwawrzen <grzegorz.wawrzen@protonmail.com>
2025-05-24 20:50:56 +0200

    chore: initialize project

---
+++ vocabulary-read-service/src/main/java/com/learnwords/vocabularyreadservice/VocabularyReadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learnwords.vocabularyreadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabularyReadServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VocabularyReadServiceApplication.class, args);
+    }
+
+}
