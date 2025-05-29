ddf3e5cf3256f88b9c6f8ab21054e32d0cbfa13b
grzegorzwawrzen <grzegorz.wawrzen@protonmail.com>
2025-05-29 18:37:14 +0200

    feat: handle vocabulary events with outbox

--- vocabulary-command-service/src/main/java/com/learnwords/vocabularycommandservice/service/VocabularyService.java
+++ vocabulary-command-service/src/main/java/com/learnwords/vocabularycommandservice/service/VocabularyService.java
@@ -1,5 +1,6 @@
 package com.learnwords.vocabularycommandservice.service;
 
+import com.learnwords.common.AggregateType;
 import com.learnwords.common.EventType;
 import com.learnwords.common.KafkaGroup;
 import com.learnwords.common.KafkaTopic;
@@ -26,54 +27,58 @@ import java.util.stream.Collectors;
 @Slf4j
 @Service
 public class VocabularyService {
-    private final KafkaTemplate<String, VocabularyDto> vocabularyDtoKafkaTemplate;
-    private final KafkaTemplate<String, SentenceDto> sentenceDtoKafkaTemplate;
     private final OutboxRepository outboxRepository;
     private final EntityToOutboxEntityMapper entityToOutboxEntityMapper;
     private final SentenceService sentenceService;
 
-    public VocabularyService(KafkaTemplate<String, VocabularyDto> vocabularyDtoKafkaTemplate, KafkaTemplate<String, SentenceDto> sentenceDtoKafkaTemplate, OutboxRepository outboxRepository, EntityToOutboxEntityMapper entityToOutboxEntityMapper, SentenceService sentenceService){
-        this.vocabularyDtoKafkaTemplate = vocabularyDtoKafkaTemplate;
-        this.sentenceDtoKafkaTemplate = sentenceDtoKafkaTemplate;
+    public VocabularyService(OutboxRepository outboxRepository, EntityToOutboxEntityMapper entityToOutboxEntityMapper, SentenceService sentenceService){
         this.outboxRepository = outboxRepository;
         this.entityToOutboxEntityMapper = entityToOutboxEntityMapper;
         this.sentenceService = sentenceService;
     }
 
-//    public VocabularyDto createVocabulary(CreateVocabularyDto createVocabularyDto) {
-//        log.debug("Rozpoczęcie tworzenia słówka: {}", createVocabularyDto.getWord());
-//        List<String> sentenceIds = new ArrayList<>();
-//
-//            if (createVocabularyDto.getSentences() != null && !createVocabularyDto.getSentences().isEmpty()) {
-//                try {
-//                    for (CreateSentenceDto createSentenceDto : createVocabularyDto.getSentences()){
-//                        SentenceDto sentenceDto = sentenceService.createSentence(createSentenceDto);
-//                        sentenceIds.add(sentenceDto.id());
-//                        log.info("Stworzono nowe zdania: {}", sentenceDto);
-//                    }
-//                } catch (Exception e) {
-//                    log.error("Błąd podczas zapisywania zdań: {}", e.getMessage(), e);
-//                    throw new RuntimeException("Nie udało się zapisać zdań", e);
-//                }
-//            }
-//            try {
-//                String id = UUID.randomUUID().toString();
-//                VocabularyDto vocabularyDto = new VocabularyDto(
-//                        id,
-//                        createVocabularyDto.getWord(),
-//                        createVocabularyDto.getTranslations(),
-//                        sentenceIds
-//                );
-//                outboxRepository.save(entityToOutboxEntityMapper.map(id, vocabularyDto, EventType.CREATE_VOCABULARY));
-//                vocabularyDtoKafkaTemplate.send(KafkaTopic.CREATE_VOCABULARY_TOPIC, vocabularyDto);
-//                log.info("Stworzono słówko: ID: {}, słowo: '{}', tłumaczenia: {}, powiązane zdania: {}",
-//                        vocabularyDto.id(), vocabularyDto.word(), vocabularyDto.translations(), vocabularyDto.sentenceIds());
-//                return vocabularyDto;
-//            } catch (Exception e) {
-//                log.error("Błąd podczas zapisywania słówka: {}", e.getMessage(), e);
-//                throw new RuntimeException("Nie udało się zapisać słówka", e);
-//            }
-//        }
+    public VocabularyDto createVocabulary(CreateVocabularyDto createVocabularyDto) {
+        log.debug("Rozpoczęcie tworzenia słówka: {}", createVocabularyDto.getWord());
+        List<String> sentenceIds = new ArrayList<>();
+
+            if (createVocabularyDto.getSentences() != null && !createVocabularyDto.getSentences().isEmpty()) {
+                try {
+                    for (CreateSentenceDto createSentenceDto : createVocabularyDto.getSentences()){
+                        SentenceDto sentenceDto = sentenceService.createSentence(createSentenceDto);
+                        sentenceIds.add(sentenceDto.id());
+                        log.info("Stworzono nowe zdania: {}", sentenceDto);
+                    }
+                } catch (Exception e) {
+                    log.error("Błąd podczas zapisywania zdań: {}", e.getMessage(), e);
+                    throw new RuntimeException("Nie udało się zapisać zdań", e);
+                }
+            }
+            try {
+                String aggregateId = UUID.randomUUID().toString();
+                VocabularyDto eventPayload = new VocabularyDto(
+                        aggregateId,
+                        createVocabularyDto.getWord(),
+                        createVocabularyDto.getTranslations(),
+                        sentenceIds
+                );
+
+                Outbox outbox = entityToOutboxEntityMapper.map(
+                        aggregateId,
+                        AggregateType.VOCABULARY,
+                        eventPayload,
+                        EventType.CREATE_VOCABULARY
+                );
+
+                outboxRepository.save(outbox);
+
+                log.info("Stworzono słówko: ID: {}, słowo: '{}', tłumaczenia: {}, powiązane zdania: {}",
+                        eventPayload.id(), eventPayload.word(), eventPayload.translations(), eventPayload.sentenceIds());
+                return eventPayload;
+            } catch (Exception e) {
+                log.error("Błąd podczas zapisywania słówka: {}", e.getMessage(), e);
+                throw new RuntimeException("Nie udało się zapisać słówka", e);
+            }
+        }
 
     }
 
