465101d2390ca88ee14ffe06f65485b7806fe81a
grzegorzwawrzen <grzegorz.wawrzen@protonmail.com>
2025-05-29 18:37:15 +0200

    feat: handle vocabulary events with outbox

--- common/src/main/java/com/learnwords/common/KafkaTopic.java
+++ common/src/main/java/com/learnwords/common/KafkaTopic.java
@@ -2,6 +2,6 @@ package com.learnwords.common;
 
 public class KafkaTopic {
     public static final String CREATE_SENTENCE_TOPIC = "outbox.event.SENTENCE";
-    public static final String CREATE_VOCABULARY_TOPIC = "CREATE_VOCABULARY";
+    public static final String CREATE_VOCABULARY_TOPIC = "outbox.event.VOCABULARY";
     public static final String UPPATED_STATUS = "UPDATED_STATUS";
 }
