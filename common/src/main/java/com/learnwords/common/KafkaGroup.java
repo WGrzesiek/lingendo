2cfeb60be189615dab30f826f15b5a577acc5d9d
grzegorzwawrzen <grzegorz.wawrzen@protonmail.com>
2025-07-13 22:35:09 +0200

    chore: update flashcard functionality

--- common/src/main/java/com/learnwords/common/KafkaGroup.java
+++ common/src/main/java/com/learnwords/common/KafkaGroup.java
@@ -3,5 +3,5 @@ package com.learnwords.common;
 public class KafkaGroup {
     public static final String VOCABULARY_READ_SERVICE_GROUP = "sentence-service-group";
     public static final String VOCABULARY_COMMAND_GROUP = "vocabulary-command-service-group";
-    public static final String DECK_GROUP = "deck-service-group";
+    public static final String DECK_SERVICE_GROUP = "deck-service-group";
 }
--- common/src/main/java/com/learnwords/common/dto/VocabularyDto.java
+++ common/src/main/java/com/learnwords/common/dto/VocabularyDto.java
@@ -2,5 +2,8 @@ package com.learnwords.common.dto;
 
 import java.util.List;
 
-public record VocabularyDto(String id, String word, List<String> translations, List<String> sentenceIds) {
+public record VocabularyDto(String id, String word, List<String> translations, List<String> sentenceIds, String deckId) {
+    public VocabularyDto(String id, String word, List<String> translations, List<String> sentenceIds) {
+        this(id, word, translations, sentenceIds, null);
+    }
 }
