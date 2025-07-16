986218250bae5f83de94f7cd48033b592cf96151
grzegorzwawrzen <grzegorz.wawrzen@protonmail.com>
2025-07-16 22:19:02 +0200

    chore: update session flow

---
+++ vocabulary-read-service/src/main/java/com/learnwords/vocabularyreadservice/dto/OnlyWordDto.java
@@ -0,0 +1,4 @@
+package com.learnwords.vocabularyreadservice.dto;
+
+public record OnlyWordDto(String id, String word) {
+}
--- vocabulary-read-service/src/main/java/com/learnwords/vocabularyreadservice/service/VocabularyService.java
+++ vocabulary-read-service/src/main/java/com/learnwords/vocabularyreadservice/service/VocabularyService.java
@@ -4,6 +4,7 @@ import com.learnwords.common.EventType;
 import com.learnwords.common.KafkaGroup;
 import com.learnwords.common.KafkaTopic;
 import com.learnwords.common.dto.VocabularyDto;
+import com.learnwords.vocabularyreadservice.dto.OnlyWordDto;
 import com.learnwords.vocabularyreadservice.dto.ResponseVocabularyDto;
 import com.learnwords.vocabularyreadservice.entity.Vocabulary;
 import com.learnwords.vocabularyreadservice.repository.VocabularyRepository;
@@ -12,6 +13,8 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -46,5 +49,39 @@ public class VocabularyService {
         Vocabulary vocabulary = vocabularyRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Nie znaleziono słowa o id: " + id));
         return new ResponseVocabularyDto(vocabulary.getWord(), vocabulary.getTranslations(), vocabulary.getSentenceIds());
+    }
+
+    public List<ResponseVocabularyDto> getVocabularies(List<String> ids) {
+        log.info("Pobieranie słów o id: {}", ids);
+        try {
+            List<Vocabulary> vocabularyList = vocabularyRepository.findAllById(ids);
+            return vocabularyList.stream()
+                    .map(v -> new ResponseVocabularyDto(v.getWord(), v.getTranslations(), v.getSentenceIds()))
+                    .toList();
+        } catch (DataAccessException e) {
+            log.error("Błąd dostępu do bazy danych: {}", e.getMessage(), e);
+            throw new RuntimeException("Błąd dostępu do bazy danych: " + e.getMessage(), e);
+        } catch (Exception e) {
+            log.error("Błąd podczas pobierania słów: {}", e.getMessage(), e);
+            throw new RuntimeException("Błąd podczas pobierania słów: " + e.getMessage(), e);
+        }
+    }
+
+    public List<OnlyWordDto> getOnlyWordsByIds(List<String> ids) {
+        log.info("Pobieranie słów o id: {}", ids);
+        try {
+            List<Vocabulary> vocabularyList = vocabularyRepository.findAllById(ids);
+            return vocabularyList.stream()
+                    .map(v -> new OnlyWordDto(v.getId(), v.getWord()))
+                    .toList();
+        }
+        catch (DataAccessException e) {
+            log.error("Błąd dostępu do bazy danych: {}", e.getMessage(), e);
+            throw new RuntimeException("Błąd dostępu do bazy danych: " + e.getMessage(), e);
+        } catch (Exception e) {
+            log.error("Błąd podczas pobierania słów: {}", e.getMessage(), e);
+            throw new RuntimeException("Błąd podczas pobierania słów: " + e.getMessage(), e);
+        }
+
     }
 }
