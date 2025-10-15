928ca0e0723c20910d62a151cf54872ac0e678ea
grzegorzwawrzen <grzegorz.wawrzen@protonmail.com>
2025-10-15 23:42:11 +0200

    fix: address follow-up issue

--- vocabulary-command-service/src/main/java/com/learnwords/vocabularycommandservice/security/SecurityConfig.java
+++ vocabulary-command-service/src/main/java/com/learnwords/vocabularycommandservice/security/SecurityConfig.java
@@ -1,40 +1,19 @@
 package com.learnwords.vocabularycommandservice.security;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.oauth2.server.resource.web.BearerTokenResolver;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
-    private String jwkSetUri;
-
     @Bean
     public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable());
-//                .sessionManagement(sess -> sess
-//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .authorizeHttpRequests(auth -> auth
-//                        .anyRequest().authenticated())
-//                .oauth2ResourceServer(oauth2 -> oauth2
-//                        .jwt(jwt -> jwt
-//                                .jwkSetUri(jwkSetUri)
-//                        )
-//                );
-
         return http.build();
     }
-
-
 }
