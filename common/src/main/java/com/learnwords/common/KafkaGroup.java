400a0b88946f694a70680e22285f287eefd719b1
grzegorzwawrzen <grzegorz.wawrzen@protonmail.com>
2025-10-28 21:05:50 +0100

    feat: add outbox integration

--- common/src/main/java/com/learnwords/common/KafkaGroup.java
+++ common/src/main/java/com/learnwords/common/KafkaGroup.java
@@ -4,4 +4,5 @@ public class KafkaGroup {
     public static final String VOCABULARY_READ_SERVICE_GROUP = "sentence-service-group";
     public static final String VOCABULARY_COMMAND_GROUP = "vocabulary-command-service-group";
     public static final String DECK_SERVICE_GROUP = "deck-service-group";
+    public static final String OUTBOX_EVENT_SERVICE_GROUP = "outbox-event-service-group";
 }
--- common/src/main/java/com/learnwords/common/KafkaTopic.java
+++ common/src/main/java/com/learnwords/common/KafkaTopic.java
@@ -3,5 +3,5 @@ package com.learnwords.common;
 public class KafkaTopic {
     public static final String CREATE_SENTENCE_TOPIC = "outbox.event.SENTENCE";
     public static final String CREATE_VOCABULARY_TOPIC = "outbox.event.VOCABULARY";
-    public static final String UPPATED_STATUS = "UPDATED_STATUS";
+    public static final String UPPATED_STATUS = "outbox.event.UPDATE_STATUS";
 }
