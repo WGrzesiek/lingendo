455c57992cd4810d70842a993c0980c7e1d5ba22
grzegorzwawrzen <grzegorz.wawrzen@protonmail.com>
2025-10-29 17:39:08 +0100

    chore: update project changes

--- vocabulary-command-service/src/main/java/com/learnwords/vocabularycommandservice/repository/OutboxRepository.java
+++ vocabulary-command-service/src/main/java/com/learnwords/vocabularycommandservice/repository/OutboxRepository.java
@@ -1,7 +1,17 @@
 package com.learnwords.vocabularycommandservice.repository;
 
+import com.learnwords.common.EventStatus;
 import com.learnwords.vocabularycommandservice.entity.Outbox;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OutboxRepository extends JpaRepository<Outbox, String> {
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Outbox o SET o.eventStatus = :eventStatus WHERE o.aggregateId = :aggregateId")
+    void updateOutboxEventStatus(@Param("aggregateId") String aggregate_id, @Param("eventStatus") String eventStatus);
 }
---
+++ vocabulary-command-service/src/main/java/com/learnwords/vocabularycommandservice/service/Impl/UpdateOutboxEventImpl.java
@@ -0,0 +1,34 @@
+package com.learnwords.vocabularycommandservice.service.Impl;
+
+import com.learnwords.common.KafkaGroup;
+import com.learnwords.common.KafkaTopic;
+import com.learnwords.common.dto.UpdateOutboxEventDto;
+import com.learnwords.vocabularycommandservice.repository.OutboxRepository;
+import com.learnwords.vocabularycommandservice.service.UpdateOutboxEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+public class UpdateOutboxEventImpl implements UpdateOutboxEvent {
+
+    private final OutboxRepository outboxRepository;
+
+    public UpdateOutboxEventImpl(OutboxRepository outboxRepository) {
+        this.outboxRepository = outboxRepository;
+    }
+
+    @Override
+    @Transactional
+    @KafkaListener(topics = KafkaTopic.UPPATED_STATUS, groupId = KafkaGroup.OUTBOX_EVENT_SERVICE_GROUP, containerFactory = "vocabularyKafkaListenerFactory")
+    public void updateOutboxEvent(UpdateOutboxEventDto updateOutboxEventDto) {
+        try{
+            outboxRepository.updateOutboxEventStatus(updateOutboxEventDto.aggregateId(), updateOutboxEventDto.eventStatus().name());
+        } catch (Exception e){
+            throw new RuntimeException("Błąd podczas aktualizacji statusu eventu w outbox: " + e.getMessage(), e);
+        }
+    }
+
+}
