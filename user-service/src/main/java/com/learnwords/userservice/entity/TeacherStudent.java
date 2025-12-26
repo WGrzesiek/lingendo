1b97d75d7800dd14b390a33ff8600e9c71f0e362
grzegorzwawrzen <grzegorz.wawrzen@protonmail.com>
2025-12-26 15:38:06 +0100

    feat: implement teacher functionality

---
+++ user-service/src/main/java/com/learnwords/userservice/entity/TeacherStudent.java
@@ -0,0 +1,55 @@
+package com.learnwords.userservice.entity;
+
+import com.learnwords.userservice.enums.TeacherStudentStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "teacher_student")
+public class TeacherStudent {
+
+    @Id
+    private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "teacher_id", nullable = false)
+    private User teacher;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "student_id", nullable = false)
+    private User student;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private TeacherStudentStatus status;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column
+    private Instant acceptedAt;
+
+    @Column(nullable = false)
+    private Instant updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        Instant now = Instant.now();
+        if (createdAt == null) {
+            createdAt = now;
+        }
+        updatedAt = createdAt;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = Instant.now();
+    }
+}
