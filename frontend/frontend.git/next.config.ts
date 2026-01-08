c5123ce93ab6364346f78de9d3efec3b388ce360
grzegorzwawrzen <grzegorz.wawrzen@protonmail.com>
2026-01-08 19:09:08 +0100

    fix: update tests

--- frontend/frontend.git/next.config.ts
+++ frontend/frontend.git/next.config.ts
@@ -10,8 +10,11 @@ import type { NextConfig } from "next";
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-    typescript: { ignoreBuildErrors: true },
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
 export default nextConfig;
+
