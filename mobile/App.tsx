1cf910201f5669de6cbb26878ab9d55b0dcf4c52
grzegorzwawrzen <grzegorz.wawrzen@protonmail.com>
2026-01-17 11:26:13 +0100

    chore: update project changes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,12 +2,15 @@ import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
+import { Button } from './components/nativewindui/Button';
+import { Text } from './components/nativewindui/Text';
 
 export default function App() {
   return (
     <>
       <ScreenContent title="Homeeee" path="App.tsx"></ScreenContent>
       <StatusBar style="auto" />
+
     </>
   );
 }
