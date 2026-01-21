b8ed85a04e0355c716ab72ca3937a87b151fb485
grzegorzwawrzen <grzegorz.wawrzen@protonmail.com>
2026-01-21 23:04:28 +0100

    chore: update outbox integration

--- vocabulary-read-service/src/main/java/com/learnwords/vocabularyreadservice/repository/VocabularyRepository.java
+++ vocabulary-read-service/src/main/java/com/learnwords/vocabularyreadservice/repository/VocabularyRepository.java
@@ -2,8 +2,16 @@ package com.learnwords.vocabularyreadservice.repository;
 
 import com.learnwords.vocabularyreadservice.entity.Vocabulary;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.Update;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface VocabularyRepository extends MongoRepository<Vocabulary, String> {
+    
+    @Query("{ '_id': ?0 }")
+    @Update("{ '$push': { 'sentenceAIds': { '$each': ?1 } } }")
+    void addSentenceAIIds(String wordId, List<String> sentenceAIIds);
 }
