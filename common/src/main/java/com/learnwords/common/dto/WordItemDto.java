30a903767b5cba9d005d7115ed4869e80516b618
grzegorzwawrzen <grzegorz.wawrzen@protonmail.com>
2026-01-27 22:53:33 +0100

    fix: adjust incoming Kafka event handling

--- common/src/main/java/com/learnwords/common/dto/WordItemDto.java
+++ common/src/main/java/com/learnwords/common/dto/WordItemDto.java
@@ -1,13 +1,18 @@
 package com.learnwords.common.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 
 import java.util.List;
 
 @Builder
 public record WordItemDto(
+    @JsonProperty("word_id")
     String wordId,
+    @JsonProperty("word")
     String word,
-    List<String > translations
+    @JsonProperty("translations")
+    List<String> translations
 ) {
 }
+
