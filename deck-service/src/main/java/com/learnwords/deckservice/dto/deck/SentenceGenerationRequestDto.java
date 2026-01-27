56692a8e2beb971aaf9e9f30fc4ce331ed218377
grzegorzwawrzen <grzegorz.wawrzen@protonmail.com>
2026-01-27 23:00:17 +0100

    fix: adjust incoming Kafka event handling

--- deck-service/src/main/java/com/learnwords/deckservice/dto/deck/SentenceGenerationRequestDto.java
+++ deck-service/src/main/java/com/learnwords/deckservice/dto/deck/SentenceGenerationRequestDto.java
@@ -1,5 +1,6 @@
 package com.learnwords.deckservice.dto.deck;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.learnwords.common.dto.WordItemDto;
 import lombok.Builder;
 
@@ -7,13 +8,20 @@ import java.util.List;
 
 @Builder
 public record SentenceGenerationRequestDto(
+    @JsonProperty("id")
     String id,
+    @JsonProperty("requested_by_user_id")
     String requestedByUserId,
+    @JsonProperty("words")
     List<WordItemDto> words,
+    @JsonProperty("level")
     String level,
+    @JsonProperty("category")
     String category,
+    @JsonProperty("language_from")
     String languageFrom,
+    @JsonProperty("language_to")
     String languageTo
-
 ) {
 }
+
